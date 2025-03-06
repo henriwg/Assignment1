91db532373da19185d3774315e903dbf79943abe
henri <henriwg@stud.ntnu.no>
2025-03-06 15:21:41 +0100

    moved server

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"CloudTechnologiesHenrikWG/server"
+	"CloudTechnologiesHenrikWG/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
-	mux := server.SetupRouter()
+	mux := SetupRouter()
 	log.Println("Server is starting on port 8080...")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
+}
+
+func SetupRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+	// Register routes (prefix patterns for paths with parameters)
+	mux.HandleFunc("/countryinfo/v1/info/", handlers.GetCountryInfo)
+	mux.HandleFunc("/countryinfo/v1/population/", handlers.GetPopulation)
+	mux.HandleFunc("/countryinfo/v1/status", handlers.GetStatus)
+	return mux
 }
